a886676c0a3424ead65a7acf2e872ea43310bee8
cartagena <jcartagena93@gmail.com>
2016-08-13 15:08:47 -0400

    updates query builder

--- app/components/query-builder.js
+++ app/components/query-builder.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  queryObject: {
+  "primaryScope": "patients",
+  "conditions": [
+    {
+      "type": "appointments",
+      "startDate": "2016-08-13",
+      "endDate": "2016-08-13"
+    },
+    {
+      "type": "balance",
+      "condition": ">",
+      "value": 0
+    }
+  ]
+},
+
+  actions: {
+    submit() {
+      this.sendAction("action", this.get("queryObject"));
+    }
+  }
 });
--- app/routes/query.js
+++ app/routes/query.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  actions: {
+    getResult(queryObject) {
+      // Ember.Logger.debug(queryObject);
+      this.transitionTo('query.result', { queryParams: {query: $.param(queryObject) }});
+    }
+  }
 });
