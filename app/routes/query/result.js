f53cae7f62a3e02ea2a07fa097c370456e78b221
Sean Sellek <me@seansellek.com>
2016-08-14 10:33:55 -0400

    fix ssl

--- app/routes/query/result.js
+++ app/routes/query/result.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     console.log(decodeURIComponent(params.query));
-    return $.getJSON('https://localhost:3000/query?' + params.query);
+    return $.getJSON('http://localhost:3000/query?' + params.query);
   },
 
   queryParams: {
